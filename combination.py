a1e85b0d896bf881d33d3462fc83a282e1d241ed
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2022-04-06 13:42:52 -0400

    tested

--- combination.py
+++ combination.py
@@ -1,16 +1,18 @@
-#!/usr/bin/env python3.8.9
-from sklearn.model_selection import train_test_split
-from functions import generate_combinations
-
-import itertools
 import h5py
+import itertools
+from functions import generate_combinations
+from sklearn.model_selection import train_test_split
 
+print("Downloading files")
 combinations = generate_combinations()
 
 print("Splitting...")
 hf = h5py.File('dataset/dataset_raw.h5', 'r')
 images = hf['images']
 labels = hf['spectra']
+
+old_training_split = None
+old_poly = None
 
 for id, combination in enumerate(combinations):
 
@@ -22,14 +24,14 @@ for id, combination in enumerate(combinations):
     epochs = combination[5]
     current_training_split = combination[6]
 
-    old_training_split = None
-    old_poly = None
+    if old_training_split != current_training_split:
+        print(
+            f"Changing split data old:{old_training_split} new:{combination[6]}")
+        old_training_split = current_training_split
 
-    if old_training_split is not current_training_split:
-        print("Changing split data")
-
-    if old_poly is not current_poly:
-        print("Changing poly count")
+    if old_poly != current_poly:
+        print(f"Changing poly count old:{old_poly} new:{combination[0]}")
+        old_poly = current_poly
 
 
 def model(id: int, polys: int, networks: int, layers: int, bias: bool, epochs: int, training_split: float):
